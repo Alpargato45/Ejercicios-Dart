5b4b398aa832ae2a960ba52d27ee20933a322751
Jorge del Cid <jorgedelcid2004@gmail.com>
2024-09-13 12:42:48 +0200

    Variables

---
+++ Ejercicios/2-Variables.dart
@@ -0,0 +1,13 @@
+void main() {
+  String nombre = "Jorge";
+  int edad = 19;
+  double altura = 1.87;
+  bool esEstudiante = false;
+
+  print("Nombre: $nombre");
+  print("Edad: $edad");
+  print("Altura: $altura");
+  print("¿Es estudiante?: $esEstudiante");
+
+  print("Nombre: $nombre, Edad: $edad, Altura: $altura, ¿Es estudiante?: $esEstudiante");
+}
